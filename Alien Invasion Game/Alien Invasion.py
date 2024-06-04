0d10b4c2b61274a89dba0a706ccd93c51fa49bab
M-K4SH1F <159590221+M-K4SH1F@users.noreply.github.com>
2024-06-04 03:14:33 -0300

    Add Alien Invasion game

---
+++ Alien Invasion Game/Alien Invasion.py
@@ -0,0 +1,157 @@
+print(r'''
+* * * * * * * * * * * * * * * * * * * * * * * * * *
+   * * * * * * * * * * * * * * * * * * * * * * * * *
+
+  "We only have room
+   for one specimen,       "Let us take the small,
+   Dtlxvr. Which shall     high-decible one, Ftxbp.
+      we take?"               It has less mass."
+             \  _.-'~~~~'-._   /
+      .      .-~ \__/  \__/ ~-.         .
+           .-~   (oo)  (oo)    ~-.
+          (_____//~~\\//~~\\______)
+     _.-~`                         `~-._
+    /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\     *
+    \___________________________________/
+               \x x x x x x x/
+       .  *     \x_x_x_x_x_x/
+  jro
+
+  @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+   @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+''')
+print("\t\t\tWELCOME TO ALIEN INVASION!\n")
+print("\t\t\t__________INSTRUCTIONS__________ ")
+print("-> Your MISSION is to escape yourself from getting abducted from aliens.")
+print("-> You will have to navigate yourself by answering the questions.")
+print("-> <choose your answers WiSeLy.>\n")
+
+print("\t\t\t   **START**       ")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if choice3 == "red":
+            print(r'''
+            
+    ,.   (   .      )        .      "
+   ("     )  )'     ,'        )  . (`     '`
+ .; )  ' (( (" )    ;(,     ((  (  ;)  "  )"
+ _"., ,._'_.,)_(..,( . )_  _' )_') (. _..( '..jb
+
+\n\n
+            ''')
+            print("It's a room full of fire.\n\t !GAME OVER! \n\t GET GOOD BOT.")
+        elif choice3 == "yellow":
+            print("You have escaped the Alien Invasion!\n\t *YOU WIN* \n\t GGS")
+        elif choice3 == "blue":
+            print("You enter a room to the roof and the Alien has caught you!")
+            print(r'''
+            
+          .                            *
+                    _.-'~~~~'-._
+      .          .-~ \__/  \__/ ~-.         .
+               .-~   (oo)  (oo)    ~-.
+              (_____//~~\\//~~\\______)
+         _.-~`                         `~-._
+        /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\
+        \___________________________________/
+                   \x x x x x x x/
+                    \x_x_x_x_x_x/
+                     :|. | ' ."
+                     | |. '. |:| Zzuzz!
+                     | :  |:. ;|
+    "Ohmygosh, a     . | : .|  :
+    flying saucer!   | . | : .|| Zzuzz!
+    - !Delores! -"   : |. | . ::  
+        \    ___     |.: |. :. |
+            /  _)    | :|___ |:|
+           |  (~o   (\.|(___)  /)
+           _\/_/_    \\(_O O_)//   
+          /      \   .\(_\O/_)/:
+         / /    \ \  :|\/\_/\/ |
+         \ |    | /  | .(_ _): |
+          \|____|/   .| /   \ |:
+           |_  _|    | /_____\.| !!!EEEEKKKK!!!
+           | |  |    : .| | | .
+           | |  |    .  | | | ||
+           |_|__|    | :|_|_| .:
+   jro     [__[__)   .: <_|_> .| 
+            \n
+            ''')
+            print("\n\t !GAME OVER! \n\t GET GOOD BOT.")
+        else:
+            print("OMG YOU THOUGHT YOU WERE COOL CHOOSING SOMETHING OUT OF THE BOX O_O! \n \t SIKE \nYou chose a door that doesn't exist.\n\t !GAME OVER! \n\t GET GOOD BOT.")
+    else:
+        print("You entered an open ground and the Alien has caught you!")
+        print(r'''
+            
+          .                            *
+                    _.-'~~~~'-._
+      .          .-~ \__/  \__/ ~-.         .
+               .-~   (oo)  (oo)    ~-.
+              (_____//~~\\//~~\\______)
+         _.-~`                         `~-._
+        /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\
+        \___________________________________/
+                   \x x x x x x x/
+                    \x_x_x_x_x_x/
+                     :|. | ' ."
+                     | |. '. |:| Zzuzz!
+                     | :  |:. ;|
+    "Ohmygosh, a     . | : .|  :
+    flying saucer!   | . | : .|| Zzuzz!
+    - !Delores! -"   : |. | . ::  
+        \    ___     |.: |. :. |
+            /  _)    | :|___ |:|
+           |  (~o   (\.|(___)  /)
+           _\/_/_    \\(_O O_)//   
+          /      \   .\(_\O/_)/:
+         / /    \ \  :|\/\_/\/ |
+         \ |    | /  | .(_ _): |
+          \|____|/   .| /   \ |:
+           |_  _|    | /_____\.| !!!EEEEKKKK!!!
+           | |  |    : .| | | .
+           | |  |    .  | | | ||
+           |_|__|    | :|_|_| .:
+   jro     [__[__)   .: <_|_> .| 
+            \n
+            ''')
+        print("\n\t !GAME OVER! \n\t GET GOOD BOT.")
+else:
+    print("You entered an open ground and the Alien has caught you!")
+    print(r'''
+            
+          .                            *
+                    _.-'~~~~'-._
+      .          .-~ \__/  \__/ ~-.         .
+               .-~   (oo)  (oo)    ~-.
+              (_____//~~\\//~~\\______)
+         _.-~`                         `~-._
+        /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\
+        \___________________________________/
+                   \x x x x x x x/
+                    \x_x_x_x_x_x/
+                     :|. | ' ."
+                     | |. '. |:| Zzuzz!
+                     | :  |:. ;|
+    "Ohmygosh, a     . | : .|  :
+    flying saucer!   | . | : .|| Zzuzz!
+    - !Delores! -"   : |. | . ::  
+        \    ___     |.: |. :. |
+            /  _)    | :|___ |:|
+           |  (~o   (\.|(___)  /)
+           _\/_/_    \\(_O O_)//   
+          /      \   .\(_\O/_)/:
+         / /    \ \  :|\/\_/\/ |
+         \ |    | /  | .(_ _): |
+          \|____|/   .| /   \ |:
+           |_  _|    | /_____\.| !!!EEEEKKKK!!!
+           | |  |    : .| | | .
+           | |  |    .  | | | ||
+           |_|__|    | :|_|_| .:
+   jro     [__[__)   .: <_|_> .| 
+            \n
+            ''')
+    print("\n\t !GAME OVER! \n\t GET GOOD!")
