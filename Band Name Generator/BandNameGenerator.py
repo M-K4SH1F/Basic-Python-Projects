7b25559fbe826216235eda950e94fc00c10a2379
M-K4SH1F <159590221+M-K4SH1F@users.noreply.github.com>
2024-06-04 03:13:02 -0300

    Add Band Name Generator script

---
+++ Band Name Generator/BandNameGenerator.py
@@ -0,0 +1,11 @@
+#Greeting for the program
+print("Welcome to the Brand Name Generator.")
+
+#Asks the user for their favourite color.
+color = input("What's your favourite color?\n")
+
+#Asks user for the name of their favorite animal.
+animal = input("What's your favourite animal?\n")
+
+#Combines the name of their favourite color and animal and show them their band name.
+print("Your band name could be " + color + " " + animal)
